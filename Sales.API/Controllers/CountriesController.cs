981db42ef78a10b57b807fd7bc346c37a75901af
Michael Arjel <marjel@ecomsur.com>
2023-07-14 22:27:34 -0400

    Begin contries APi Controller.

---
+++ Sales.API/Controllers/CountriesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+using System.Runtime.CompilerServices;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController:ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Countries.ToList());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+    }
+}
